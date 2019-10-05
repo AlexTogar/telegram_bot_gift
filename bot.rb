3861b17b1514e463195b10d8b55827f62d5c77c5
AlexTogar <smirnov.togar@ya.ru>
2019-10-05 17:50:07 +0300

    debug

--- bot.rb
+++ bot.rb
@@ -16,22 +16,22 @@ require 'telegram/bot'
 token = ENV["bubu_token"]
 alex_chat_id = 479_039_553
 
-    # Telegram::Bot::Client.run(token) do |bot|
-    #     bot.listen do |message|
-    #         chat_id = message.chat.id
+    Telegram::Bot::Client.run(token) do |bot|
+        bot.listen do |message|
+            chat_id = message.chat.id
 
-    #         # if message.text == "/schedule" then
-    #         #     bot.api.send_message(chat_id: chat_id, text: "расписание))")
-    #         # end
-    #         bot.api.send_message(chat_id: chat_id, text: "ПрИвЕтиКи, я пока нипанимаю нисево, но туть скоро буит мб расписание0)0Буб")
-    #     end
-    # end
+            # if message.text == "/schedule" then
+            #     bot.api.send_message(chat_id: chat_id, text: "расписание))")
+            # end
+            bot.api.send_message(chat_id: chat_id, text: "ПрИвЕтиКи, я пока нипанимаю нисево, но туть скоро буит мб расписание0)0Буб")
+        end
+    end
 
 # loop do
 
-    Telegram::Bot::Client.run(token) do |bot|
-        bot.api.send_message(chat_id: alex_chat_id, text: "привет, сейчас: #{Time.now}")
-    end
+    # Telegram::Bot::Client.run(token) do |bot|
+    #     bot.api.send_message(chat_id: alex_chat_id, text: "привет, сейчас: #{Time.now}")
+    # end
 
     # sleep(10)
 
